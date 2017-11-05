81ebc1bdf9df2b50b02bd137c7db3bbe44633286
lmlsna <lmlsna@lavabit.com>
2017-11-05 15:36:58 -0600

    ported to python3, code commenting and cleanup, added error handling, readme changes, added systemd service file

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ python eap_proxy.py <ONT_INTERFACE_NAME> <MODEM_INTERFACE_NAME>
 Example: python eap_proxy.py eth0 eth1
 
 This should run on an EdgerouterLite and many other devices without issue. You may need to restart dhcp for your ethernet device on your WAN to get this working(After it's proxied all the EAP packets the first time). You may need to create a vlan 0 on your ONT device to be your WAN, and you almost certainly have to clone the mac of your modem on the ONT device interface. I'm terrible at documentation, so good luck.
+
+# eap_proxy.service
+To run this script as a systemd service:
+
+* Copy `eap_proxy.service` to `/etc/systemd/system/`
+* run the commands `systemctl enable eap_proxy; systemctl start eap_proxy`
+* Make sure you change the `YOUR_ONT` and `YOUR_INT` values to the correct iface names and that the `eap_proxy.service` file is calling the correct locations of this script and your python binary.
+
+# Python3
+This script has been ported to Python 3, but should still be compatible with earlier versions.
--- eap_proxy.py
+++ eap_proxy.py
@@ -1,67 +1,96 @@
+#!/usr/bin/python3
+
 import socket
 from threading import Thread
 import signal
 import ctypes
 import fcntl
 import sys
+import datetime
 
 class Sniffer():
-
+    # Structure for ifreq interface flags
     class ifreq(ctypes.Structure):
         _fields_ = [("ifr_ifrn", ctypes.c_char * 16),
                     ("ifr_flags", ctypes.c_short)]
 
-
+    # On init, tries to create and bind to a raw socket on
+    # each of the given interfaces, then sets promiscuous mode
     def __init__(self, iface_ont, iface_int):
-        self.s_ont=socket.socket(socket.PF_PACKET,socket.SOCK_RAW,socket.htons(0x888e))
-        self.s_ont.bind((iface_ont,0))
-        self.s_int=socket.socket(socket.PF_PACKET,socket.SOCK_RAW,socket.htons(0x888e))
-        self.s_int.bind((iface_int,0))
-        self.sniff = True
-        self.promisc(True)
+        try:
+            # Try to create and bind a socket on the ONT port
+            self.s_ont=socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x888e))
+            self.s_ont.bind((iface_ont,0))
+        except socket.error as err:
+            # Print error and exit with error code on failure
+            print("Could not create socket on " + iface_ont + ": " + err.args[1])
+            sys.exit(err.args[0])
 
+        try:
+            # Try to create and bind a socket on the INT port
+            self.s_int=socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.htons(0x888e))
+            self.s_int.bind((iface_int,0))
+        except socket.error as err :
+            # Print error and exit with error code on failure
+            print("Could not create socket on " + iface_ont + ": " + err.args[1])
+            sys.exit(err.args[0])
+
+        self.sniff = True
+        # Put both interfaces into promsicuous mode
+        self.promisc(self.s_ont, True)
+        self.promisc(self.s_int, True)
+
+    # Loop that listens for EAP type (0x888E) ether frames
+    # arriving at the source socket (s_sock) and forwards
+    # that data to the destination socket (d_sock).
+    #
+    # This function will be called by two seperate threads,
+    # each listing on and sending to opposite sockets to
+    # effectively create a bidirectional ethernet proxy.
     def proxy(self, s_sock, d_sock):
-        print('Sniffing on: ' + s_sock.getsockname()[0])
-        while(self.sniff):
+        src = s_sock.getsockname()[0]
+        dst = d_sock.getsockname()[0]
+
+        print( "Started sniffing for EAP packets on " + src)
+        fails = 0
+        while( self.sniff ):
             pkt = s_sock.recv(2048)
             d_sock.send(pkt)
-            print('Sent ' + str(len(pkt)) + ' bytes')
+            print( "Relayed " + str(len(pkt)) + " byte EAP packet from " + src + " => " + dst)
 
-    def promisc(self, tog):
+    # Put an interface into promiscuous mode
+    def promisc(self, iface, tog):
+        # Define consts
         IFF_PROMISC = 0x100
         SIOCGIFFLAGS = 0x8913
         SIOCSIFFLAGS = 0x8914
 
-        # ONT Promisc
+        # Create an instance of the defined ifreq structure
         ifr = self.ifreq()
-        ifr.ifr_ifrn = self.s_ont.getsockname()[0]
-        fcntl.ioctl(self.s_ont.fileno(), SIOCGIFFLAGS, ifr)
-        if tog:
-            ifr.ifr_flags |= IFF_PROMISC
-        else:
-            ifr.ifr_flags &= ~IFF_PROMISC
-        fcntl.ioctl(self.s_ont.fileno(), SIOCSIFFLAGS, ifr)
+        ifr.ifr_ifrn = iface.getsockname()[0].encode() # s_o/int
+        fcntl.ioctl(iface.fileno(), SIOCGIFFLAGS, ifr)
 
-        # INT Pro
-        ifr.ifr_ifrn = self.s_int.getsockname()[0]
-        fcntl.ioctl(self.s_int.fileno(), SIOCGIFFLAGS, ifr)
-        if tog:
+        if tog: # Set promiscuous mode flag
             ifr.ifr_flags |= IFF_PROMISC
-        else:
+        else: # Unset promiscuous mode flag
             ifr.ifr_flags &= ~IFF_PROMISC
-        fcntl.ioctl(self.s_int.fileno(), SIOCSIFFLAGS, ifr)
+        # Apply flags to interface
+        fcntl.ioctl(iface.fileno(), SIOCSIFFLAGS, ifr)
 
 
 def signal_handler(signal, frame):
-    print 'Caught signal, exiting...'
+    print('Caught signal, exiting...')
     snf.sniff = False
+    snf.promisc(snf.s_ont,False)
+    snf.promisc(snf.s_int,False)
+
     for thread in threads:
         if thread.isAlive():
             try:
                 thread._Thread__stop()
             except:
-                print(str(thread.getName()) + ' could not be terminated')
-    snf.promisc(False)
+                print((str(thread.getName()) + ' could not be terminated'))
+                sys.exit(1)
     sys.exit(0)
 
 
@@ -74,4 +103,14 @@ if __name__ == "__main__":
 
     for t in threads: t.start()
     signal.signal(signal.SIGINT, signal_handler)
-    signal.pause()
+    #signal.pause() # Not sure why this was here, prevented
+                    # the program from exiting and would hang
+                    # if running as a daemon, preventing restart.
+                    # Maybe it was @todo for proper locking
+                    # on the threads so it could survive the
+                    # interfaces going up and down?
+                    #
+                    # In any case, I've changed the behavior
+                    # to unset promisc and exit on any socket
+                    # errors so systemd can reatart it and
+                    # keep it running as a service.
